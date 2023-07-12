71f15882a0bbe5734ef36a9597319495bd80a1f5
Goby56 <60710855+Goby56@users.noreply.github.com>
2023-07-12 14:13:39 +0200

    removed satin
    too much trouble

--- TestMod/src/main/java/me/x150/testmod/Handler.java
+++ TestMod/src/main/java/me/x150/testmod/Handler.java
@@ -3,7 +3,6 @@ package me.x150.testmod;
 import lombok.SneakyThrows;
 import me.x150.renderer.font.FontRenderer;
 import me.x150.renderer.objfile.ObjFile;
-import me.x150.renderer.render.OutlineFramebuffer;
 import me.x150.renderer.render.Renderer2d;
 import me.x150.renderer.render.Renderer3d;
 import me.x150.renderer.util.RendererUtils;
@@ -27,7 +26,6 @@ public class Handler {
 					ObjFile.ResourceProvider.ofPath(TestMod.getAssetsDir().resolve("models").resolve("custom").resolve("nuke")));
 		}
 		ob.draw(stack, new Matrix4f(), new Vec3d(0, 100, 0));
-		OutlineFramebuffer.useAndDraw(() -> Renderer3d.renderFilled(stack, Color.WHITE, new Vec3d(0, 300, 0), new Vec3d(5, 5, 5)), 1f, Color.GREEN, Color.BLACK);
 	}
 
 
